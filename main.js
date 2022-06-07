5bddb1ca7305bef8b4022d0b74ac633c548bd2d3
somil00 <95484425+somil00@users.noreply.github.com>
2022-06-07 14:30:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 // javascript for calculator
 
-let input = document.getElementById('display')
+let input = document.getElementById('display');
+buttons = document.querySelectorAll('button');
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText; 
+        if(buttonText == "X"){
+            buttontext = '*';
+            inputValue += buttonText;
+            input.value = inputValue;
+        }
+        else if (buttonText == 'C'){
+            inputValue = "";
+            input.value = inputValue;
+        }
+        else if (buttonText == '='){
+           inputValue = eval(screenValue);
+        }
+        else {
+            inputValue += buttonText;
+            input.value = inputValue;
+        }
+    })
+
+}
